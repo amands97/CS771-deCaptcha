24a92bee6743978a8fd718814e62659765291eab
Aman Deep Singh <aman150084@gmail.com>
2017-11-01 21:55:25 +0530

    CPU version is done i guess:

--- dataLoader.py
+++ dataLoader.py
@@ -15,33 +15,31 @@ def importData(folder = "./datatext/", clip = -1):
     labels = []
     i = 0
     for file in os.listdir(folder):
+        print(file)
         if file.endswith('.jpg'):
             path = os.getcwd()
             path = path + "/datatext/" + file
             image = pil_loader(path)
-
-            image = image.resize((150, 80), Image.ANTIALIAS)
+            image = image.resize((180, 50), Image.ANTIALIAS)
             images.append(image)
             label = file.split(".")[0]
             label2 = []
             for i, j in enumerate(label):
-                if j.isdigit():
-                    j = int(j)
-                    a = np.eye(62)[j]
-                    label2.append(a)
-                    continue
                 if j.isupper():
-                    j = ord(j) - ord('A') + 10
-                    a = np.eye(62)[j]
+                    j1 = ord(j) - ord('A') + 10
+                    a = np.eye(62)[j1]
                     label2.append(a)
-                    continue
                 if j.islower():
-                    j = ord(j) - ord('a') + 36
-                    a = np.eye(62)[j]
+                    j2 = ord(j) - ord('a') + 36
+                    a = np.eye(62)[j2]
                     label2.append(a)
-            i = i + 1
-            if i == clip:
-                break
+                if j.isdigit():
+                    j3 = int(j)
+                    a = np.eye(62)[j3]
+                    label2.append(a)
             labels.append(label2)
+        i = i + 1
+        if i == clip:
+           break
 
     return (images, labels)
--- main.py
+++ main.py
@@ -34,10 +34,11 @@ class CNNNetwork(torch.nn.Module):
         # self.relu3 = nn.functional.relu(inplace = True)
         self.dropout3 = nn.Dropout(p=0.2)
         self.maxpool3 = nn.MaxPool2d(kernel_size = (2,2), stride = 2)
-        self.fc1 = nn.Linear(3072, 372)
+        # self.v = nn.View(14080)
+        self.fc1 = nn.Linear(14080, 3072)
         # self.relu4 = nn.functional.relu(inplace = True)
         self.dropout4 = nn.Dropout(p=0.2)
-        self.fc2 = nn.Linear(372, 1)
+        self.fc2 = nn.Linear(3072,372)
 
     def forward(self, x):
         out = self.conv1(x)
@@ -56,7 +57,8 @@ class CNNNetwork(torch.nn.Module):
         out = nn.functional.relu(out)
         out = self.dropout3(out)
         out = self.maxpool3(out)
-
+        # out = self.v(out)
+        out = out.view(14080)
         out = self.fc1(out)
         # out = self.relu4(out)
         out = nn.functional.relu(out)
@@ -66,7 +68,8 @@ class CNNNetwork(torch.nn.Module):
 
 model1 = CNNNetwork()
 print("model:",model1)
-images, labels = importData(folder = "./datatext/", clip = 10) # default directory is "./datatext/". set clip = -1 for accessing whole db 
+images, labels = importData(folder = "./datatext/", clip = 100) # default directory is "./datatext/". set clip = -1 for accessing whole db 
+print("sdad")
 # print(images, labels)
 criterion = nn.MultiLabelSoftMarginLoss()
 
@@ -74,16 +77,18 @@ preprocess = transforms.Compose([
 
    transforms.ToTensor()
 ])
+optimizer = torch.optim.Adam(model1.parameters(), lr = 0.0001)
 def train(images, labels, num_epochs = 10):
     losses = []
     for n in range(num_epochs):
         rloss = 0
+        images_ = []
         for i, image in enumerate(images):
             #image = image.cuda(async=True)
-            print(i)
+            # print(i)
             image = preprocess(image)
             image = Variable(image)
-            print(image)
+            # print(image)
             label = labels[i]
             label = np.asarray(label)
             label = torch.Tensor(label)
@@ -93,7 +98,8 @@ def train(images, labels, num_epochs = 10):
             #print("labels", labels.size())
             image = image.unsqueeze(0)
             pred_out = model1(image)
-            loss = criterion(pred_out, labels)
+            # print(label, pred_out)
+            loss = criterion(pred_out, label)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -107,4 +113,4 @@ def train(images, labels, num_epochs = 10):
 
 
 
-train(images, labels, 1)
+train(images, labels, 10)
